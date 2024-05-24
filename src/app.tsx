d792932b345fed4f26be8bf1be0be982b4988a5b
João Gabriel <joaogabrielbochlivio@gmail.com>
2024-05-24 00:26:23 -0300

    seção 'projetos' e alguns ajustes

--- src/app.tsx
+++ src/app.tsx
@@ -3,6 +3,8 @@ import { Separator } from '@/components/ui/separator'
 import { ProfileInfo } from '@/components/profile-info'
 import { Header } from '@/components/header'
 import { Badge } from '@/components/ui/badge'
+import { Project } from '@/components/project'
+import { SquareTerminal } from 'lucide-react'
 
 export function App() {
   return (
@@ -13,14 +15,26 @@ export function App() {
       <Separator className='max-w-[75%] lg:max-w-5xl my-3 mx-auto' />
       <div className='max-w-[75%] lg:max-w-5xl mx-auto'>
         <Badge className='mb-3'>Sobre mim</Badge>
+        <p className='mb-3'>
+          Desenvolvedor de software, apaixonado por resolver problemas através da tecnologia. Estou cursando o 5º
+          semestre de Sistemas de Informação na FHO - Uniararas.
+        </p>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur aliquam id reprehenderit aperiam atque
-          tempora animi nostrum, dolores ex facilis autem recusandae quidem sit doloremque nobis earum ipsa numquam et!
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati neque dolor distinctio velit dolore esse
-          asperiores repellendus, ullam incidunt unde quas ad vel nam nostrum quis quisquam, accusantium ipsum quidem.
+          Atualmente, estou concentrando meus estudos no desenvolvimento para web e estou aberto a novas oportunidades de trabalho.
         </p>
       </div>
       <Separator className='max-w-[75%] lg:max-w-5xl my-3 mx-auto' />
+      <div className='max-w-[75%] lg:max-w-5xl mx-auto'>
+        <Badge className='mb-3'>Projetos</Badge>
+        <Project
+          title={'juao.dev'}
+          logo={SquareTerminal}
+          description={'Meu portfólio pessoal, a página que você esta acessando no momento :D'}
+          imagePath={'/portfolio.png'}
+          projectUrl={'https://juao.dev'}
+          githubUrl={'https://github.com/juaoxd/juao.dev'}
+        />
+      </div>
     </ThemeProvider>
   )
 }
---
+++ src/components/project.tsx
@@ -0,0 +1,63 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog'
+import { Card, CardDescription, CardHeader, CardTitle } from './ui/card'
+import { Badge } from './ui/badge'
+import { Github, Globe, LucideIcon } from 'lucide-react'
+import React from 'react'
+
+interface ProjectProps {
+  title: string
+  logo: LucideIcon
+  description: string
+  imagePath: string
+  projectUrl: string
+  githubUrl: string
+}
+
+export function Project({ title, logo, description, imagePath, projectUrl, githubUrl }: ProjectProps) {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Card className='max-w-64 hover:cursor-pointer'>
+          <CardHeader>
+            <div className='flex gap-3'>
+              {React.createElement(logo)}
+              <CardTitle>{title}</CardTitle>
+            </div>
+            <CardDescription>{description}</CardDescription>
+          </CardHeader>
+        </Card>
+      </DialogTrigger>
+      <DialogContent className='sm:max-w-[425px]'>
+        <DialogHeader>
+          <DialogTitle>{title}</DialogTitle>
+          <DialogDescription>{description}</DialogDescription>
+        </DialogHeader>
+        <img src={imagePath} alt='' />
+        <DialogFooter>
+          <div className='flex justify-end w-full gap-2'>
+            <a href={projectUrl} target='_blank' rel='noopener noreferrer'>
+              <Badge className='flex gap-1'>
+                Link
+                <Globe />
+              </Badge>
+            </a>
+            <a href={githubUrl} target='_blank' rel='noopener noreferrer'>
+              <Badge className='flex gap-1'>
+                Código
+                <Github />
+              </Badge>
+            </a>
+          </div>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  )
+}
